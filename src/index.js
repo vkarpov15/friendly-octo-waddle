05236adb4e03d2f2d6621db235ed36ab646947e0
Valeri Karpov <valkar207@gmail.com>
2016-05-15 11:30:19 -0700

    step 3: redux middleware + HTTP

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import superagent from 'superagent';
 
 const defaultState = { checked: false };
 const reducer = function(state = defaultState, action) {
   switch (action.type) {
-    case 'TOGGLE':
-      return Object.assign({}, state, { checked: !state.checked });
+    case 'LOAD_ARTICLES':
+      return Object.assign({}, state, {
+        articlesCount: action.payload.articlesCount
+      });
   }
   return state;
 };
-const store = createStore(reducer);
+
+const promiseMiddleware = store => next => action => {
+  if (action.payload && typeof action.payload.then === 'function') {
+    return action.payload.then(
+      res => {
+        action.payload = res.body;
+        next(action);
+      },
+      error => {
+        action.error = true;
+        action.payload = error;
+        next(action);
+      }
+    );
+  }
+  next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
+
+const ARTICLE_URL =
+  'https://conduit.productionready.io/api/articles?limit=10&offset=0';
 
 class App extends React.Component {
   constructor() {
@@ -23,20 +47,19 @@ class App extends React.Component {
   }
 
   render() {
-    const onClick = () => store.dispatch({ type: 'TOGGLE' });
+    const onClick = () => store.dispatch({
+      type: 'LOAD_ARTICLES',
+      payload: superagent.get(ARTICLE_URL)
+    });
     return (
       <div>
-        <h1>To-dos</h1>
+        <h1>Articles</h1>
         <div>
-          Learn Redux&nbsp;
-          <input
-            type="checkbox"
-            checked={!!this.state.checked}
-            onClick={onClick} />
+          <button onClick={onClick}>Load Articles</button>
         </div>
-        {
-          this.state.checked ? (<h2>Done!</h2>) : null
-        }
+        <div>
+          Number of Articles: {this.state.articlesCount}
+        </div>
       </div>
     );
   }
